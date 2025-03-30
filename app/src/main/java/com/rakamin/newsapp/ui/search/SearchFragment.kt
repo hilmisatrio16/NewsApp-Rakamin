d97dbe68bb071ca39333d3cffb2fcf95fd388c2c
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2025-03-30 16:50:06 +0700

    feat: slicing layout and setup fragment

--- app/src/main/java/com/rakamin/newsapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/rakamin/newsapp/ui/search/SearchFragment.kt
@@ -1,4 +1,4 @@
-package com.rakamin.newsapp.ui.home
+package com.rakamin.newsapp.ui.search
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,26 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.rakamin.newsapp.R
-import com.rakamin.newsapp.databinding.FragmentHomeBinding
+import com.rakamin.newsapp.databinding.FragmentSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class SearchFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-       _binding = FragmentHomeBinding.inflate(layoutInflater, container,false)
+        _binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/rakamin/newsapp/ui/search/SearchViewModel.kt
@@ -0,0 +1,4 @@
+package com.rakamin.newsapp.ui.search
+
+class SearchViewModel {
+}
